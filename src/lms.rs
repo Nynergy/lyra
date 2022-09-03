f0b41fb827c2b0ca2b98cb7f978a6a5a697a7b18
Ben Buchanan <bsbuchanan99@gmail.com>
2022-09-03 17:04:33 -0400

    Added helper methods for retrieving playlist info

--- src/lms.rs
+++ src/lms.rs
@@ -45,10 +45,12 @@ pub struct LmsResponse {
 
 impl LmsResponse {
     // NOTE: Used for debugging only
+    #[allow(dead_code)]
     pub fn dump(&self) {
         println!("{:?}", self.result);
     }
 
+    #[allow(dead_code)]
     pub fn get_u64(&self, key: &str) -> Result<u64, String> {
         if let Some(value) = self.result.get(key) {
             if value.is_u64() {
@@ -60,4 +62,75 @@ impl LmsResponse {
             Err(format!("'{}' does not exist!", key))
         }
     }
+
+    #[allow(dead_code)]
+    pub fn get_str(&self, key: &str) -> Result<String, String> {
+        if let Some(value) = self.result.get(key) {
+            if value.is_string() {
+                Ok(value.as_str().unwrap().to_string())
+            } else {
+                Err(format!("'{}' is not a string!", key))
+            }
+        } else {
+            Err(format!("'{}' does not exist!", key))
+        }
+    }
+
+    pub fn get_array(
+        &self,
+        key: &str
+    ) -> Result<&Vec<serde_json::Value>, String> {
+        if let Some(value) = self.result.get(key) {
+            if value.is_array() {
+                Ok(value.as_array().unwrap())
+            } else {
+                Err(format!("'{}' is not an array!", key))
+            }
+        } else {
+            Err(format!("'{}' does not exist!", key))
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn get_object(
+        &self,
+        key: &str
+    ) -> Result<&serde_json::Map<String, serde_json::Value>, String> {
+        if let Some(value) = self.result.get(key) {
+            if value.is_object() {
+                Ok(value.as_object().unwrap())
+            } else {
+                Err(format!("'{}' is not an object!", key))
+            }
+        } else {
+            Err(format!("'{}' does not exist!", key))
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LmsPlayer {
+    pub name: String,
+    pub playerid: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LmsPlaylist {
+    pub tracks: Vec<LmsSong>,
+}
+
+impl LmsPlaylist {
+    pub fn from(tracks: Vec<LmsSong>) -> Self {
+        Self { tracks, }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LmsSong {
+    #[serde(rename = "playlist index")]
+    pub index: u64,
+    pub title: String,
+    pub artist: String,
+    pub album: String,
+    pub duration: f64,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,88 @@
-// A basic usage example showing how to get the number of players
-// currently connected to the LMS.
-
 mod lms;
 
 use lms::*;
 
+struct App {
+    client: LmsClient,
+    player: Option<LmsPlayer>,
+}
+
+impl App {
+    async fn from(server_str: String) -> Result<Self, reqwest::Error> {
+        let client = LmsClient::from(server_str);
+        let command = serde_json::json!(["-", ["serverstatus", 0]]);
+        let res = client.query(command)
+            .await?
+            .json::<LmsResponse>()
+            .await?;
+
+        let players = res.get_array("players_loop")
+            .expect("Could not extract value");
+        let player: Option<LmsPlayer>;
+        if !players.is_empty() {
+            player = Some(
+                // For now, we just connect to the first player we find
+                serde_json::from_str(&players[0].to_string())
+                .unwrap());
+        } else {
+            player = None;
+        }
+
+        Ok(Self { client, player })
+    }
+
+    fn get_current_playerid(&self) -> Option<String> {
+        if let Some(player) = &self.player {
+            Some(player.playerid.clone())
+        } else {
+            None
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let client = LmsClient::from("192.168.0.188:9000".to_string());
+    let app = App::from("192.168.0.188:9000".to_string()).await?;
 
-    let command = serde_json::json!(["-", ["serverstatus"]]);
+    if let Some(playerid) = app.get_current_playerid() {
+        // Determine how many tracks there are in the current playlist
+        let command =
+            serde_json::json!([playerid, ["status", 0, 9999, "tags:adl"]]);
 
-    let res = client.query(command)
-        .await?
-        .json::<LmsResponse>()
-        .await?;
+        let res = app.client.query(command)
+            .await?
+            .json::<LmsResponse>()
+            .await?;
 
-    let count = res.get_u64("player count")
-        .expect("Could not extract value");
-    println!("Player Count: {}", count);
+        let playlist_loop = res.get_array("playlist_loop")
+            .expect("Could not extract value");
+        let mut playlist: Vec<LmsSong> = Vec::new();
+        for track in playlist_loop.iter() {
+            playlist.push(serde_json::from_str(&track.to_string()).unwrap());
+        }
+        let playlist = LmsPlaylist::from(playlist);
 
-    res.dump();
+        for track in playlist.tracks.iter() {
+            println!(
+                "{}: {} - {} - {} ({})",
+                track.index + 1,
+                track.title,
+                track.artist,
+                track.album,
+                format_time(track.duration)
+            );
+        }
+    } else {
+        println!("Could not connect to a player.");
+    }
 
     Ok(())
+}
+
+fn format_time(duration: f64) -> String {
+    let minutes = duration as u64 / 60;
+    let seconds = duration as u64 % 60;
+    let time_str = format!("{}:{}", minutes, seconds);
+
+    time_str
 }
