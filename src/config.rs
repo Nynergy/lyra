786b062de580fb946f23867779036b9bfb3d3478
Ben Buchanan <bsbuchanan99@gmail.com>
2022-09-08 13:24:04 -0400

    Fallback to default colors if missing from config

--- src/config.rs
+++ src/config.rs
@@ -5,11 +5,14 @@ use std::collections::HashMap;
 pub struct Config {
     pub lms_ip: String,
     pub lms_port: String,
+    #[serde(default = "Config::default_colors")]
     pub colors: HashMap<String, u8>,
+    #[serde(default = "Config::default_colors")]
+    default_colors: HashMap<String, u8>,
 }
 
 impl Config {
-    pub fn default() -> Self {
+    fn default_colors() -> HashMap<String, u8> {
         let mut colors = HashMap::new();
         colors.insert("Banner".to_string(), 2);
         colors.insert("PlayerName".to_string(), 1);
@@ -25,18 +28,30 @@ impl Config {
         colors.insert("TrackDuration".to_string(), 6);
         colors.insert("PlaybarGauge".to_string(), 2);
 
+        colors
+    }
+
+    pub fn default() -> Self {
         Self {
             lms_ip: "127.0.0.1".to_string(),
             lms_port: "9000".to_string(),
-            colors,
+            colors: Config::default_colors(),
+            default_colors: Config::default_colors(),
         }
     }
 
     pub fn color(&self, name: &str) -> &u8 
     {
         self.colors.get(name)
+            .unwrap_or_else(|| {
+                self.default_color(name)
+            })
+    }
+
+    fn default_color(&self, name: &str) -> &u8 {
+        self.default_colors.get(name)
             .expect(
-                &format!("'{}' is not a valid color name", name)
+                &format!("'{}' is not a valid config option", name)
             )
     }
 }
